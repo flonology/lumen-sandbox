38ce31f30f169a3977fdfe6708294192027b271b
Florian Hansen <hansen@mailbox.org>
2018-10-21 10:46:40 +0200

    Make creds downloadable

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -1,16 +1,19 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use Date;
 
 class BackupController extends Controller
 {
     public function downloadBackup(Request $request)
     {
         $user = app('auth')->user();
+        $creds = $user->creds()->get();
+        $file_name = date('Y_m_d') . '_CredsBackup.json';
 
-        return response()->json([
-            'data' => 'This is backup'
-        ]);
+        return response($creds, 200, [
+           'Content-Type' => 'application/json',
+           'Content-Disposition' => "attachment; filename=\"{$file_name}\""
+       ]);
     }
 }
